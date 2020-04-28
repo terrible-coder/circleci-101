0cfd8e834f0ddc70fd80b526833780ab914873fb
Ayanava De <ayanavade01@gmail.com>
2020-04-28 19:12:55 +0530

    meaningless harmless change

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
 export function add(a: number, b: number) {
 	return a + b;
 }
+
+/*
+This is a completely meaningless change. Nothing should break. CircleCI is not
+supposed to deploy anything to npm either.
+*/
