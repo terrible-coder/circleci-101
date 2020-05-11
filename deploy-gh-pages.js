a18bba8d9f4ae15cb813e7dc729b87d14c8fbac1
Ayanava De <ayanavade01@gmail.com>
2020-05-11 22:36:26 +0530

    adding script file for gh-pages

---
+++ deploy-gh-pages.js
@@ -0,0 +1,15 @@
+const ghpages = require("gh-pages");
+
+ghpages.publish("docs", {
+	dotfiles: true,
+	silent: false,
+	repo: `https://${process.env.GH_TOKEN}@github.com/terrible-coder/circleci-101.git`,
+	user: {
+		name: "Ayanava De",
+		email: "ayanavade01@gmail.com"
+	},
+	message: "Deploy to gh-pages"
+}, err => {
+	console.log("Something went wrong.");
+	console.error(err);
+});
