428b9a421199fad5355a706c7e07ee5b9169f20a
Ayanava De <ayanavade01@gmail.com>
2020-07-14 21:46:42 +0530

    feat: :sparkles: add subtract function
    adding a function to subtract 2 numbers

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ export function add(a: number, b: number) {
 	return a + b;
 }
 
+export function sub(a: number, b: number) {
+	return a - b;
+}
+
 /*
 This is a completely meaningless change. Nothing should break. CircleCI is not
 supposed to deploy anything to npm either.
